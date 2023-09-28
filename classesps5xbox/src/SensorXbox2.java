c7fff3e9603a430a8fad64a63c19a739278b71ea
prearjg <94556239+prearjg@users.noreply.github.com>
2023-09-27 21:24:38 -0300

    codigo java de classes xbox e ps5

---
+++ classesps5xbox/src/SensorXbox2.java
@@ -0,0 +1,8 @@
+// Classe adaptada (Adaptee)
+public class SensorXbox2 {
+
+    //Solicitação Especifica
+    public void conectarXbox2(){
+        System.out.println("Um novo controle foi conectado ao sensor do Xbox.");
+    }
+}
