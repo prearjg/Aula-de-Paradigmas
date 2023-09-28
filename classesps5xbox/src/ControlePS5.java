a72c97c239732c241252c046f52c4f023f51b7ea
prearjg <94556239+prearjg@users.noreply.github.com>
2023-09-27 21:20:09 -0300

    Add files via upload
    Código java das classes xbox e ps5

---
+++ classesps5xbox/src/ControlePS5.java
@@ -0,0 +1,9 @@
+//Classe Cliente(Client)
+public class ControlePS5 {
+    private SensorPS5 sensorAQueSeConecta;
+
+    public void Conectar(SensorPS5 sensor){
+        this.sensorAQueSeConecta = sensor;
+        sensorAQueSeConecta.conectarPS5();
+    }
+}
---
+++ classesps5xbox/src/SensorPS5.java
@@ -0,0 +1,8 @@
+// Classe alvo (Target)
+public class SensorPS5 {
+
+    //Solicitação
+    public void conectarPS5(){
+        System.out.println("Um novo controle foi conectado ao sensor do PS5.");
+    }
+}
